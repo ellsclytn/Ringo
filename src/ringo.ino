7887e9c360f2cf03a40583cd5ca6ef8437e6786d
Ellis Clayton <ellis@ellisclayton.info>
2016-09-01 09:23:07 +1000

    Blink, and remain not blinking for random periods

--- src/ringo.ino
+++ src/ringo.ino
@@ -8,13 +8,17 @@ void setup() {
 }
 
 void loop() {
+  delay(random(0, 300) * 1000);
   blinkLoop(902.255638);
 }
 
 void blinkLoop(double rate) {
+  long onPeriod = random(5, 45) * 1000 + millis();
+  while (millis() < onPeriod) {
     int randNumber = random(0,2);
     digitalWrite(LEDS[randNumber], HIGH);
     delay((short) (rate / LED_COUNT - 50));
     digitalWrite(LEDS[randNumber], LOW);
     delay(50);
+  }
 }
